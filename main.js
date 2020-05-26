a0d30d8d56338507bf13426aaa412d6e8955ca89
Pablo Novas <pjnovas@gmail.com>
2020-05-26 11:22:00 -0300

    added support for external links

--- main.js
+++ main.js
@@ -1,25 +1,13 @@
-const { app, globalShortcut, BrowserWindow } = require('electron');
+const { app, globalShortcut, BrowserWindow, shell } = require('electron');
 const path = require('path');
 
-function createWindow() {
-  const mainWindow = new BrowserWindow({
-    width: 1000,
-    height: 800,
-    webPreferences: {
-      partition: 'persist:sitePoller'
-    }
-  });
-
-  const sub = process.env.WORKSPACE ? `${process.env.WORKSPACE}.` : '';
-
-  mainWindow.loadURL(`https://${sub}slack.com/`);
-  mainWindow.menuBarVisible = false;
-
+const initSideBarToggle = win => {
   let visible = true;
+
   const toggleSidebar = () => {
     visible = !visible;
 
-    mainWindow.webContents.executeJavaScript(`
+    win.webContents.executeJavaScript(`
       document.querySelectorAll(
         '.p-workspace--iap1.p-workspace--context-pane-collapsed'
       )[0].style.gridTemplateColumns = '${visible ? '220px auto' : '0px auto'}';
@@ -33,6 +21,31 @@ function createWindow() {
   app.on('browser-window-blur', () => {
     globalShortcut.unregister('CommandOrControl+Tab');
   });
+};
+
+const initExternalLinks = win => {
+  win.webContents.on('new-window', function (e, url) {
+    e.preventDefault();
+    shell.openExternal(url);
+  });
+};
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 800,
+    webPreferences: {
+      partition: 'persist:sitePoller'
+    }
+  });
+
+  const sub = process.env.WORKSPACE ? `${process.env.WORKSPACE}.` : '';
+
+  mainWindow.loadURL(`https://${sub}slack.com/`);
+  mainWindow.menuBarVisible = false;
+
+  initSideBarToggle(mainWindow);
+  initExternalLinks(mainWindow);
 
   if (process.env.NODE_ENV === 'development') {
     mainWindow.webContents.openDevTools({ mode: 'detach' });
